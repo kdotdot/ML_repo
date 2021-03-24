f27094675dddbe0b9bc9b7bb25e89bf794154a87
carlosg <carlos.gomeztapia@gmail.com>
2021-03-24 11:34:12 +0100

    Better but more work needed

--- dog or cat - pytorch/main.py
+++ dog or cat - pytorch/main.py
@@ -3,6 +3,7 @@ import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets, transforms
 from einops import rearrange, reduce, repeat
@@ -10,28 +11,29 @@ from einops import rearrange, reduce, repeat
 class Net(nn.Module):
     def __init__(self):
         super(Net,self).__init__()
-        self.conv1 = nn.Conv2d(3,16,7)
-        self.pool1 = nn.MaxPool2d(2,2)
+        self.conv1 = nn.Conv2d(3,16,3)
+        self.conv2 = nn.Conv2d(16,32,3,3)
+        self.conv3 = nn.Conv2d(32,64,5,3)
 
-        self.conv2 = nn.Conv2d(16,32,7,3)
-        self.pool2 = nn.MaxPool2d(2,2)
+        self.pool = nn.MaxPool2d(2,2)
+        self.dropout = nn.Dropout(p=0.15)
+        self.fc1 = nn.Linear(256, 512)
+        self.fc2 = nn.Linear(512,32)
+        self.fc3 = nn.Linear(32,1)
 
-        self.fc1 = nn.Linear(32 * 17 * 17,120)
-        self.fc2 = nn.Linear(120,100)
-        self.fc3 = nn.Linear(100,2)
-
-        self.finalact = nn.LogSoftmax(dim=1)
 
     # Add dropout
     # Add pooling
     def forward(self,x):
-        x = self.pool1(F.relu(self.conv1(x)))
-        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.dropout(x)
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
         x = rearrange(x,'n c h w -> n (c h w)')
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
-        return self.finalact(x)
+        return x.view(-1) 
 
 if __name__ == '__main__':
     transformations = transforms.Compose([
@@ -53,17 +55,30 @@ if __name__ == '__main__':
                                           shuffle=True)
 
     model = Net()
-    #optimizer = torch.optim.Adam(model.parameters(),lr = 0.0001)
-    optimizer = torch.optim.SGD(model.parameters(), lr = 0.0001, momentum = 0.9)
-    loss_fct = nn.NLLLoss()
-    for epoch in range(3):
+    optimizer = torch.optim.Adam(model.parameters())
+    #optimizer = torch.optim.SGD(model.parameters(), lr = 0.0001, momentum = 0.9)
+    loss_fct = nn.BCEWithLogitsLoss()
+    for epoch in range(10):
         model.train()
         for batch in train_loader:
             X = batch[0]
-            y = batch[1].long()
+            y = batch[1]
             optimizer.zero_grad()
             out = model(X)
+            out = out.double() 
+            y = y.double()
             loss= loss_fct(out,y)
             loss.backward()
             optimizer.step()
-            print(f'Epoch: {epoch} Loss: {loss.item()}')
+            print(f'Epoch: {epoch} Train loss: {loss.item()}')
+    model.eval()
+    losses = []
+    for batch in test_loader:
+        X = batch[0]
+        y = batch[1]
+        out = model(X)
+        out = out.double() 
+        y = y.double()
+        loss= loss_fct(out,y)
+        losses.append(loss.item())
+    print(f'Test loss: {np.array(losses).mean()}')
